b140914c1100580a7d6ad5b822f7aab6b4fd8aa9
Amayuru1999 <amarasingheau@gmail.com>
2025-12-06 12:30:43 +0530

    feat: basic ts function defining

--- main.ts
+++ main.ts
@@ -3,3 +3,13 @@ function greet(name:string): string{
 }
 const message: string = greet("Amayuru");
 console.log(message);
+
+// let username ="alice";
+// let score= 99;
+// let flags =[true,false,true];
+//
+// function add(a:number,b:number){
+//     return a+b;
+// }
+//
+// console.log(add(1,4));
