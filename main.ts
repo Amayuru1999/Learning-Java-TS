79b6563d05beec4553644572821b25b6aa4387f2
Amayuru1999 <amarasingheau@gmail.com>
2025-12-06 12:31:19 +0530

    feat: type inference and explicit type

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-function greet(name:string): string{
-    return `Hello, ${name}`;
-}
-const message: string = greet("Amayuru");
-console.log(message);
-
-// let username ="alice";
-// let score= 99;
-// let flags =[true,false,true];
-//
-// function add(a:number,b:number){
-//     return a+b;
+// function greet(name:string): string{
+//     return `Hello, ${name}`;
 // }
-//
-// console.log(add(1,4));
+// const message: string = greet("Amayuru");
+// console.log(message);
+
+let username ="alice";
+let score= 99;
+let flags =[true,false,true];
+
+function add(a:number,b:number){
+    return a+b;
+}
+
+console.log(add(1,4));
