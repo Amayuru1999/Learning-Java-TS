c5c568c943292965964515fd371c2f4207280ca5
Amayuru1999 <amarasingheau@gmail.com>
2025-12-07 21:33:59 +0530

    feat: for loop in Java

--- Main.java
+++ Main.java
@@ -159,12 +159,20 @@
 //    }
 //}
 
+//public class Main{
+//    public static void main(String[] args){
+//        int i = 0;
+//        while(i<5){
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+//}
+
 public class Main{
     public static void main(String[] args){
-        int i = 0;
-        while(i<5){
-            System.out.println(i);
-            i++;
+        for (int i = 0; i<5; i++){
+            System.out.println("Value of i:" + i);
         }
     }
 }
